67170a83e19d6961e3e19c742f06fda9a1b590bc
Leo <Leo1987tw@gmail.com>
2026-04-21 13:58:08 +0800

    迴圈基本介紹

---
+++ 03_forloop.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>重覆結構</title>
+</head>
+<body>
+
+<h3>For...loop 練習</h3>
+
+<?php
+
+for($i=1; $i<=10; $i=$i+1){
+    echo "$i =>";
+    echo $i*10 . "<br>";
+}
+
+?>
+
+</body>
+</html>
