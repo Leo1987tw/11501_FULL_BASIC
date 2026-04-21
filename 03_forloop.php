9ce31b6efe23d630bb396354ada773e3292a8876
Leo <Leo1987tw@gmail.com>
2026-04-21 14:41:34 +0800

    迴圈應用介紹

--- 03_forloop.php
+++ 03_forloop.php
@@ -9,6 +9,8 @@
 
 <h3>For...loop 練習</h3>
 
+<h4>列出一到十乘以十</h4>
+
 <?php
 
 for($i=1; $i<=10; $i=$i+1){
@@ -17,6 +19,69 @@ for($i=1; $i<=10; $i=$i+1){
 }
 
 ?>
+
+<br>
+
+<h4>列出奇數</h4>
+
+<?php
+
+for($i=1; $i<=10; $i=$i+1){
+    echo "$i =>";
+    echo $i*2-1 . "<br>";
+}
+
+?>
+
+<br>
+
+<h4>列出偶數</h4>
+
+<?php
+
+for($i=1; $i<=10; $i=$i+1){
+    echo "$i =>";
+    echo $i*2-2 . "<br>";
+}
+
+?>
+
+<h3>WHILE loop 練習</h3>
+
+<?php
+
+$score=43;
+
+echo "成績為：" . $score . "分" . "<br>";
+$count=0; //計算迴圈執行次數
+while($score<60){
+    $score=$score+10;
+    $count=$count+1;
+}
+
+echo "登錄成績為" . $score . "分 <br>";
+echo "迴圈執行次數：" . $count . "次 <br>";
+
+?>
+
+<h3>foreach練習</h3>
+
+<?php
+
+$score=[80, 90, 75, 60, 55];
+
+foreach($score as $idx => $val){
+    echo "第" . ($idx+1) . "位成績為：" . $val . "分 <br>";
+}
+
+?>
+
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
 
 </body>
 </html>
